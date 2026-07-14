a39d2616546f7ffee01f6ee7cf47bddc1f3b8dc0
Teo <maestrobaldwin@outlook.com>
2026-07-14 18:59:01 +0000

    New Variable folder and exercises for LS

---
+++ variable_exercises/age.js
@@ -0,0 +1,10 @@
+let age = 20;
+let howOld = function (inThisMany) {
+  console.log(`In ${inThisMany} years, you will be ${inThisMany + age}.`);
+}
+
+console.log(`You are ${age} years old.`);
+howOld(10);
+howOld(20);
+howOld(30);
+howOld(40);
---
+++ variable_exercises/greeter.js
@@ -0,0 +1,7 @@
+const NAME = 'Victor';
+let greeting1 = 'Good Morning,';
+let greeting2 = 'Good Afternoon,';
+let greeting3 = 'Good Evening,';
+console.log(`${greeting1} ${NAME}.`);
+console.log(`${greeting2} ${NAME}.`);
+console.log(`${greeting3} ${NAME}.`);
