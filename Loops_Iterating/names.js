09f9ed8c8ec4676f1781274b19eada34d689c8c2
Teo Baldwin <maestrobaldwin@outlook.com>
2026-07-25 13:35:47 -0400

    Updated names script

--- Loops_Iterating/names.js
+++ Loops_Iterating/names.js
@@ -1,3 +1,4 @@
+/* while loop
 let names = ['Chris', 'Kevin', 'Naveed', 'Pete', 'Victor',];
 let upperNames = [];
 let index = 0;
@@ -7,5 +8,18 @@ while (index < names.length) {
   upperNames.push(upperCaseName);
   index += 1;
 }
+console.log(names);
+console.log(upperNames);
+*/
+
+// for loop
+let names = ['Tony', 'Sam', 'Gina', 'Alfie', 'Todd',];
+let upperNames = [];
+for (let i = 0; i < names.length; i += 1){
+  let capName = names[i].toUpperCase();
+  upperNames.push(capName);
+  console.log(i);
+}
+
 console.log(names);
 console.log(upperNames);
