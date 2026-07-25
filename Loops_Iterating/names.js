0eb4835f14d482a9b11993a7424ef7a193c83cff
Teo Baldwin <maestrobaldwin@outlook.com>
2026-07-25 17:33:33 -0400

    Updated names file with more examples

--- Loops_Iterating/names.js
+++ Loops_Iterating/names.js
@@ -13,6 +13,7 @@ console.log(upperNames);
 */
 
 // for loop
+/*
 let names = ['Tony', 'Sam', 'Gina', 'Alfie', 'Todd',];
 let upperNames = [];
 for (let i = 0; i < names.length; i += 1){
@@ -23,3 +24,23 @@ for (let i = 0; i < names.length; i += 1){
 
 console.log(names);
 console.log(upperNames);
+*/
+
+// suppose we want all names except Alfie
+let names = ['Chris', 'Kevin', 'Alfie', 'Pete', 'Victor'];
+let upperNames = [];
+
+for (let index = 0; index < names.length; index += 1) {
+  if (names[index] === 'Alfie') {
+    continue;
+  }
+  /* or if (names[index] !== 'Alfie') {
+  let upperCaseName = names[index].toUpperCase etc. --> just run the code}
+*/
+
+  let upperCaseName = names[index].toUpperCase();
+  upperNames.push(upperCaseName);
+}
+
+console.log(upperNames);
+
