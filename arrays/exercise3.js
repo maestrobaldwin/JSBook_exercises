45801650e63a0f48c16e98d5753fa6199741af66
Teo Baldwin <maestrobaldwin@outlook.com>
2026-08-13 16:19:06 -0400

    Adding new array exercises

---
+++ arrays/exercise3.js
@@ -0,0 +1,32 @@
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+/*
+for (let i = 0; i < myArray.length; i += 1) {
+  for (let j = 0; j < myArray[i].length; j += 1){
+    if (myArray[i][j] % 2 === 0) {
+      console.log(myArray[i][j]);
+    }
+  }
+}
+*/
+/*
+for (let i = 0; i < myArray.length; i += 1) {
+  for (let j = 0; j < myArray[i].length; j += 1) {
+    let value = myArray[i][j];
+    if (value % 2 === 0) {
+      console.log(value);
+    }
+  }
+}
+*/
+myArray.forEach(nestedArray => {
+  nestedArray.forEach(value => {
+    if (value % 2 === 0) {
+      console.log(value);
+    }
+  });
+});
